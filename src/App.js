4a730b94a322c7a875eded3a4358ee7cef0bcc66
Sean Redmon <seen360@yahoo.com>
2021-10-22 11:37:02 -0400

    footer added and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Main from "./components/Main";
-import Menu from './components/Menu';
+import Menu from "./components/Menu";
+import Footer from "./components//common/Footer";
 
 function App() {
   return (
     <div className="App">
       <Menu />
       <Main />
+      <Footer />
     </div>
   );
 }

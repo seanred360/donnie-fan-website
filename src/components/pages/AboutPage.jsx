cf8ce580b7074206a8e51833b89ff6e0131cd953
Sean Redmon <seen360@yahoo.com>
2021-10-30 13:04:25 -0400

    desktop styles added for about page

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -5,39 +5,40 @@ import FadeInSection from "../utility/FadeInSection";
 const AboutPage = () => {
   return (
     <div className="about-page">
-      <div className="section-header-group">
-        <h1 className="__header">About me</h1>
-        <p className="__label">Donnie Fan</p>
+      <div className="__left-content">
+        <div className="section-header-group">
+          <h1 className="__header">About me</h1>
+          <p className="__label">Donnie Fan</p>
+        </div>
+        <section className="__section-one">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+          blanditiis excepturi delectus odio deleniti mollitia illum doloremque
+          earum repudiandae sequi illo voluptate officia, quasi debitis odit vel
+          eius,
+        </section>
+        <img
+          className="__about-photo"
+          src={aboutPhoto}
+          alt="Donnie performing at Comedy Club China"
+        />
       </div>
 
-      <FadeInSection>
-        <section>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur
-          temporibus et velit consequatur accusantium porro placeat deleniti
-          reiciendis magni, dolore rem nemo commodi animi suscipit, iusto,
-          veritatis quidem minima sint! Excepturi dolorum id doloremque
-          architecto quos modi fugiat asperiores deserunt voluptatibus ducimus
-          omnis dolorem exercitationem, accusantium temporibus. Illo, eos alias.
-        </section>
-      </FadeInSection>
-
-      <img
-        className="__about-photo"
-        src={aboutPhoto}
-        alt="Donnie performing at Comedy Club China"
-      />
-
-      <section>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-        blanditiis excepturi delectus odio deleniti mollitia illum doloremque
-        earum repudiandae sequi illo voluptate officia, quasi debitis odit vel
-        eius, eum tempore pariatur tempora sed assumenda, dolorem recusandae
-        ullam. Maiores perspiciatis harum reprehenderit. Tempora blanditiis est,
-        repellendus minima molestias excepturi culpa adipisci odit. Sunt, a.
-        Odio consequuntur omnis quo rem, officia veniam, voluptatem quidem,
-        inventore soluta obcaecati consectetur a laboriosam aspernatur magni
-        fugiat minima reiciendis quibusdam temporibus?
-      </section>
+      <div className="__right-content">
+        <FadeInSection>
+          <section className="__section-two">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo rerum
+            obcaecati vero deleniti illum eius excepturi nisi totam, et nihil,
+            nostrum, ducimus ipsam! Aliquid similique eaque sequi beatae
+            blanditiis explicabo nisi itaque, aperiam, reprehenderit reiciendis
+            nulla porro aspernatur alias recusandae dolore sapiente incidunt
+            velit? Illum a alias nesciunt delectus, adipisci provident rerum
+            error vero ut deserunt. Architecto delectus iusto quaerat nisi
+            possimus dolorem voluptatem eligendi, velit magnam quos nesciunt,
+            earum qui amet eum accusantium mollitia modi debitis incidunt
+            recusandae a fugiat ipsum magni? Voluptatem laudantium nihil impedit
+          </section>
+        </FadeInSection>
+      </div>
     </div>
   );
 };

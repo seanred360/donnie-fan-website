529144d09d9186bf9867ec666f61c0f8ca5413b6
Sean Redmon <seen360@yahoo.com>
2021-12-28 13:19:52 -0500

    Video Button styling changed

--- src/components/VideoPreviewButton.jsx
+++ src/components/VideoPreviewButton.jsx
@@ -1,18 +1,14 @@
 import React from "react";
-// import djTalking from "../images/djTalking.jpeg";
 import donnieRooftop from "../images/donnie-rooftop.jpg";
 import FadeInSection from "../components/utility/FadeInSection";
 
 const VideoPreviewButton = ({ label, linkAddress }) => {
   return (
     <FadeInSection>
-      <button
-        className="video-preview-button"
-        onClick={() => window.open(linkAddress)}
-      >
+      <a href={linkAddress} className="video-preview-button">
         <h2 className="__video-label">{label}</h2>
         <img className="__video" src={donnieRooftop} alt="Donnie performing" />
-      </button>
+      </a>
     </FadeInSection>
   );
 };

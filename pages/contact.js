4ee1af0f6ca97dea8ef53f78eb036d91e3ca9b69
Sean Redmon <seen360@yahoo.com>
2022-03-28 13:02:05 -0400

    Updated contact page
    added alternative contact methods

--- pages/contact.js
+++ pages/contact.js
@@ -4,6 +4,9 @@ import emailjs from "emailjs-com";
 import ContactOptions from "../components/common/ContactOptions";
 import ContactForm from "../components/common/ContactForm";
 import SectionHeader from "../components/common/SectionHeader";
+import { AiFillWechat, AiOutlineWeibo, AiOutlineMail } from "react-icons/ai";
+import { IoTicketSharp } from "react-icons/io5";
+import { MdPersonSearch } from "react-icons/md";
 
 const contact = (props) => {
   const contactForm = useRef();
@@ -73,6 +76,77 @@ const contact = (props) => {
           </style>
         </div>
         <ContactOptions setSelectedReason={setSelectedReason} />
+        <div className="contact-other">
+          <h3
+            style={{
+              fontSize: "24px",
+              borderBottom: "1px solid #f4c203",
+            }}
+          >
+            <span>
+              <MdPersonSearch style={{ marginRight: "10px" }} color="#f4c203" />
+              Or find me here
+            </span>
+          </h3>
+          <span>
+            <AiOutlineMail
+              style={{ marginRight: "10px" }}
+              size="32px"
+              color="#f4c203"
+            />
+            donniefancomedy@gmail.com
+          </span>
+          <span>
+            <AiFillWechat
+              style={{ marginRight: "10px" }}
+              size="32px"
+              color="#f4c203"
+            />
+            dj281444
+          </span>
+          <span>
+            <AiOutlineWeibo
+              style={{ marginRight: "10px" }}
+              size="32px"
+              color="#f4c203"
+            />
+            DONNIEFANCOMEDY
+          </span>
+          <h4
+            style={{
+              fontSize: "24px",
+              borderBottom: "1px solid #f4c203",
+            }}
+          >
+            <IoTicketSharp
+              style={{ marginRight: "10px" }}
+              size="32px"
+              color="#f4c203"
+            />
+            For tickets and other information
+          </h4>
+          <span>
+            <AiFillWechat
+              style={{ marginRight: "10px" }}
+              size="32px"
+              color="#f4c203"
+            />
+            vivicoffee66
+          </span>
+          <style jsx>
+            {`
+              div {
+                display: flex;
+                justify-content: center;
+                alighn-items: center;
+                flex-direction: column;
+                max-width: 600px;
+                margin-right: auto;
+                margin-left: auto;
+              }
+            `}
+          </style>
+        </div>
       </>
     );
   else

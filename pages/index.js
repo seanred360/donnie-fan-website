950e6b6c417c1cde6e34d1aefb5d2915e6d0261e
Sean Redmon <seen360@yahoo.com>
2022-03-29 15:16:52 -0400

    Update index.js
    Hero banner is now an img not just a background. Images are content and should be in the HTML.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Meta from "../components/Meta";
-import FadeInSection from "../components/common/utility/FadeInSection";
+// import FadeInSection from "../components/common/utility/FadeInSection";
 import SocialMediaFeed from "../components/common/SocialMediaFeed";
 import styles from "../styles/pages/Home.module.scss";
+
 
 const home = () => {
   return (
     <>
       <Meta
         title="Donnie Fan | Standup Comedy in Beijing"
-        keywords="Standup comedy in beijing donnie fan from zero to hero"
+        keywords="Standup comedy in           beijing donnie fan from zero to hero"
         description="Longtime Beijing local is doing English stand up comedy in Beijing"
       />
       <div className={styles.container}>

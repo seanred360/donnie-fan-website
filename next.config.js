9eebca60df15ba172969960981b2289528bda287
Sean Redmon <seen360@yahoo.com>
2022-11-03 16:47:05 -0400

    added favicon

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
-  },
 };
 
 module.exports = nextConfig;
